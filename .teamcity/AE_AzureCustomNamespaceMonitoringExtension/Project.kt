c4839f21cee9cf8be7369fdb4f35f9a85ff8adf5
admin <admin@TeamCity>
2020-04-07 20:04:00 +0530

    TeamCity change in 'AppDynamics Extensions / Azure CustomNamespace Monitoring Extension' project: 'Azure Build' build configuration was created

--- .teamcity/AE_AzureCustomNamespaceMonitoringExtension/Project.kt
+++ .teamcity/AE_AzureCustomNamespaceMonitoringExtension/Project.kt
@@ -1,5 +1,6 @@
 package AE_AzureCustomNamespaceMonitoringExtension
 
+import AE_AzureCustomNamespaceMonitoringExtension.buildTypes.*
 import AE_AzureCustomNamespaceMonitoringExtension.vcsRoots.*
 import AE_AzureCustomNamespaceMonitoringExtension.vcsRoots.AE_AzureCustomNamespaceMonitoringExtension_Azurecustomnamespacemonitoringextensionci
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
@@ -15,6 +16,8 @@ object Project : Project({
 
     vcsRoot(AE_AzureCustomNamespaceMonitoringExtension_Azurecustomnamespacemonitoringextensionci)
 
+    buildType(AE_AzureCustomNamespaceMonitoringExtension_AzureBuild)
+
     features {
         versionedSettings {
             id = "PROJECT_EXT_4"
@@ -26,16 +29,4 @@ object Project : Project({
             storeSecureParamsOutsideOfVcs = true
         }
     }
-
-    params {
-        password("env.CLIENT_ID", "zxx47244f4679365380172109a6f589a6c9a25e5baffe6fd6b8b7bcd0a59b7fa9b603dfba7f81e32b20")
-        password("env.CLIENT_SECRET", "zxx5e1e72372ce8ae8b045878057cb6e2021783647d9f878141600a3b41febff4be67bf471d161bb5a4")
-        password("env.SUBSCRIPTION_ID", "zxxadde6fc530cbb116801f2c087b9d7e135888f72eddeecd428d25153eef7bbc3e7bc2fb8e4c4a118f")
-        password("env.TENANT_ID", "zxxc2cd637a6b2a6cd9dcd231ce995e8ce8cbb3d383b776cca3d85464ebeb52a0ff22416ab9961de16a")
-    }
-
-    buildTypesOrder = arrayListOf(
-            AE_AzureCustomNamespaceMonitoringExtension_Build,
-            AE_AzureCustomNamespaceMonitoringExtension_IntegrationTests
-    )
 })
---
+++ .teamcity/AE_AzureCustomNamespaceMonitoringExtension/buildTypes/AE_AzureCustomNamespaceMonitoringExtension_AzureBuild.kt
@@ -0,0 +1,8 @@
+package AE_AzureCustomNamespaceMonitoringExtension.buildTypes
+
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
+
+object AE_AzureCustomNamespaceMonitoringExtension_AzureBuild : BuildType({
+    uuid = "de2e79f2-0341-497b-ab3a-c382670b111f"
+    name = "Azure Build"
+})
