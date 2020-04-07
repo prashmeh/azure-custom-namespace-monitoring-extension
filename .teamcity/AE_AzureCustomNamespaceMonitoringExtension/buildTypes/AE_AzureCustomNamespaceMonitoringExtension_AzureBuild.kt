d0606290b11ffd83ea2953368407ac2e7e22a471
admin <admin@TeamCity>
2020-04-07 20:04:16 +0530

    TeamCity change in 'AppDynamics Extensions / Azure CustomNamespace Monitoring Extension' project: VCS roots of 'Azure Build' build configuration were updated

--- .teamcity/AE_AzureCustomNamespaceMonitoringExtension/buildTypes/AE_AzureCustomNamespaceMonitoringExtension_AzureBuild.kt
+++ .teamcity/AE_AzureCustomNamespaceMonitoringExtension/buildTypes/AE_AzureCustomNamespaceMonitoringExtension_AzureBuild.kt
@@ -5,4 +5,8 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.*
 object AE_AzureCustomNamespaceMonitoringExtension_AzureBuild : BuildType({
     uuid = "de2e79f2-0341-497b-ab3a-c382670b111f"
     name = "Azure Build"
+
+    vcs {
+        root(AE_AzureCustomNamespaceMonitoringExtension.vcsRoots.AE_AzureCustomNamespaceMonitoringExtension_Azurecustomnamespacemonitoringextensionci)
+    }
 })
